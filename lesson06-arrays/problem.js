35cc2109012c74f056e1e1648108e0c0b719428a
Kevin Wong <kevin.wong954@gmail.com>
2025-01-28 00:00:03 -0500

    complete lesson06-arrays

--- lesson06-arrays/problem.js
+++ lesson06-arrays/problem.js
@@ -1,10 +1,20 @@
-// Prompt: Determine whether the largest element in the array is at least twice as much as every other number in the array. If it is, return the index of the largest element, or return -1 otherwise.
+// Prompt: Determine whether the largest element in the array is at least twice as much
+// as every other number in the array.
+// If it is, return the largest element, or return -1 otherwise.
 
 /**
  * Checks if the largest element in the array is at least twice as much as every other number.
  * @param {number[]} nums - The integer array.
  * @returns {number} - The index of the largest element if it meets the condition, otherwise -1.
  */
-function checkLargestElement(nums) {}
+function checkLargestElement(nums) {
+  let largest = Math.max(...nums)
+  for (num of nums) {
+    if (num !== largest && largest < num * 2) {
+      return -1
+    }
+  }
+  return largest
+}
 
-module.exports = checkLargestElement;
+module.exports = checkLargestElement
--- lesson06-arrays/warmup.js
+++ lesson06-arrays/warmup.js
@@ -1,3 +1,27 @@
 // 1. Counting: create a function that prints out the numbers 1-10
+function counting() {
+  for (i = 1; i <= 10; i++) {
+    console.log(i)
+  }
+}
+console.log('Counting: ')
+counting()
 // 2. Evens: create a function that prints out the even numbers 1-20
+function evens() {
+  for (let i = 1; i <= 20; i++) {
+    if (i % 2 === 0) console.log(i)
+  }
+}
+console.log('Evens: ')
+evens()
 // 3. FizzBuzz: create a function that prints out the numbers 1-100. If the number is divisible by 3, print "Fizz" instead of the number. If the number is divisible by 5, print "Buzz" instead of the number. If the number is divisible by both 3 and 5, print "FizzBuzz" instead of the number.
+function FizzBuzz() {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) console.log('FizzBuzz')
+    else if (i % 3 == 0) console.log('Fizz')
+    else if (i % 5 == 0) console.log('Buzz')
+    else console.log(i)
+  }
+}
+console.log('FizzBuzz: ')
+FizzBuzz()
