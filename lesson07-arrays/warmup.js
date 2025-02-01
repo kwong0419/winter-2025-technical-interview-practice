b73089f72c5c20ca625c895f0db5c84b3026023f
Kevin Wong <kevin.wong954@gmail.com>
2025-02-01 10:58:23 -0500

    complete lesson07-arrays warmup

--- lesson07-arrays/warmup.js
+++ lesson07-arrays/warmup.js
@@ -1,5 +1,18 @@
 // Prompt 1: Sum of Array Write a function called sumArray that takes an array of numbers as input and returns the sum of all the numbers in the array.
-
+function sumArray(arr) {
+  return arr.reduce((acc, curr) => acc + curr)
+}
+console.log(sumArray([1, 2, 3, 4, 5]))
+console.log(sumArray([1, 2, 3]))
 // Prompt 2: Find Maximum Write a function called findMax that takes an array of numbers as input and returns the maximum number in the array.
-
+function findMax(arr) {
+  return Math.max(...arr)
+}
+console.log(findMax([1, 2, 3, 4, 5]))
+console.log(findMax([1, 2, 3]))
 // Prompt 3: Reverse Array Write a function called reverseArray that takes an array as input and returns a new array with the elements in reverse order.
+function reverseArray(arr) {
+  return arr.reverse()
+}
+console.log(reverseArray([1, 2, 3, 4, 5]))
+console.log(reverseArray([1, 2, 3]))
